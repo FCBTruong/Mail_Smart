3f723a599b95dc4c07e34aba3cc66915787ec87b
Truong <55469561+FCBTruong@users.noreply.github.com>
2022-09-23 15:17:31 +0700

    update mailbox

--- src/App.js
+++ src/App.js
@@ -1,280 +1,247 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { useForm } from "react-hook-form";
 import ReactJson from "react-json-view";
 import "bootstrap/dist/css/bootstrap.css";
 import itemConfig from "./storage_item_config.json";
-import "./styles.css";
+
+import Container from "@mui/material/Container";
+import MailIcon from "@mui/icons-material/Mail";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+
 import {
   NotificationContainer,
   NotificationManager,
 } from "react-notifications";
 import "react-notifications/lib/notifications.css";
 
-var JsonConfig = {
-  gift: {
-    gold: 500000,
-    diamond: 0,
-    tickets: [],
-    items: [],
-  },
-};
-
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    shouldUnregister: true,
+  const [data, setData] = useState({
+    gift: {
+      gold: 0,
+      diamond: 0,
+      tickets: [],
+      items: [],
+    },
+    action: "",
   });
+  useEffect(() => {
+    document.title = "Mail Smart";
+  }, [data]);
 
-  const [items, setItems] = React.useState([]);
-  const [tickets, setTickets] = React.useState([]);
-  const [counter, setCounter] = React.useState(0);
-  const [counterTicket, setCounterTicket] = React.useState(0);
-  const [selectedItemType, setItemType] = React.useState({
-    type: 0,
-  });
-
-  function clickAddItem() {
-    console.log("Add item");
-    setItems((prevIndexes) => [...prevIndexes, counter]);
-    setCounter((prevCounter) => prevCounter + 1);
-    console.log(JSON.stringify(items));
-  }
-
-  const removeItem = (index) => () => {
-    console.log("INDEX_REMOVE", index);
-    setItems((prevIndexes) => [
-      ...prevIndexes.filter((item) => item !== index),
-    ]);
-    setCounter((prevCounter) => prevCounter - 1);
-    console.log(JSON.stringify(items));
+  const onChangeGold = (e) => {
+    var newData = { ...data };
+    newData.gift.gold = parseInt(e.target.value);
+    setData(newData);
   };
 
-  function clickAddTicket() {
-    console.log("Click Add ticket");
-    setTickets((prevIndexes) => [...prevIndexes, counterTicket]);
-    setCounterTicket((prevCounter) => prevCounter + 1);
-    console.log(JSON.stringify(tickets));
-  }
-
-  const removeTicket = (index) => () => {
-    console.log("INDEX_REMOVE", index);
-    setTickets((prevIndexes) => [
-      ...prevIndexes.filter((item) => item !== index),
-    ]);
-    setCounterTicket((prevCounter) => prevCounter - 1);
-    console.log(JSON.stringify(items));
+  const onChangeDiamond = (e) => {
+    var newData = { ...data };
+    newData.gift.diamond = parseInt(e.target.value);
+    setData(newData);
   };
 
-  function handleChange(e) {
-    console.log("Item type selected" + e.target.value);
-    setItemType({ ...selectedItemType, type: e.target.value });
-    console.log("Selected" + selectedItemType);
+  const onChangeAction = (e) => {
+    var newData = { ...data };
+    newData.action = e.target.value;
+    setData(newData);
+  };
+
+  const onAddTicket = (e) => {
+    var newData = { ...data };
+    newData.gift.tickets.push({ticketType: 1, ticketNum: 0});
+    setData(newData);
+  };
+
+  const onRemoveTicket = (e) => {
+    var newData = { ...data };
+    newData.gift.tickets = []
+    setData(newData);
+  };
+
+  const onChangeTicketNum = (e) => {
+    var newData = { ...data };
+    newData.gift.tickets[0].ticketNum = parseInt(e.target.value);
+    setData(newData);
   }
 
-  const onSubmit = (data) => {
-    console.log("DT: " + JSON.stringify(data));
+  const onChangeTicketType = (e) => {
+    var newData = { ...data };
+    newData.gift.tickets[0].ticketType = parseInt(e.target.value);
+    setData(newData);
+  }
 
-    // check null
-    if (data.tickets === undefined) {
-      data.tickets = [];
-    }
-    if (data.items === undefined) {
-      data.items = [];
-    }
-    // end check
+  const onAddItem = (e) => {
+    NotificationManager.info("Not available yet", ".");
+  };
 
-    var jsonResult = {};
-    jsonResult.gift = data;
-    JsonConfig = jsonResult;
-    navigator.clipboard.writeText(JSON.stringify(JsonConfig));
- 
-    NotificationManager.success('Copied to clipboard', 'Generate gifts sucessfully');
+  const onSubmit = () => {
+    navigator.clipboard.writeText(JSON.stringify(data));
+
+    NotificationManager.success(
+      "Copied to clipboard",
+      "Generate gifts sucessfully"
+    );
   }; // your form submit function which will invoke after successful validation
 
-  console.log(watch("example")); // you can watch individual input by pass the name of the input
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <p id="head_input">GOlD</p>
-      <input
-        type="number"
-        defaultValue={0}
-        {...register("gold", { valueAsNumber: true })}
-      />
-
-      <p>DIAMOND</p>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input
-        type="number"
-        defaultValue={0}
-        {...register("diamond", { required: true, valueAsNumber: true })}
-      />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <p>This field is required</p>}
-
-      <p>
-        ITEMS
-        <div className="items">
-          <ul>
-            {items.map((index) => {
-              const fieldName = `items[${index}]`;
-              return (
-                <li>
-                  <div>
-                    <p>Item ID</p>
-                    <select
-                      type="number"
-                      defaultValue={0}
-                      id="itemId"
-                      {...register(`${fieldName}.itemId`, {
-                        valueAsNumber: true,
-                      })}
-                    >
-                      {Object.keys(itemConfig[selectedItemType.type]).map(
-                        (key) => {
-                          var obj = itemConfig[selectedItemType.type][key];
-                          return <option value={obj.id}>{obj.name}</option>;
-                        }
-                      )}
-                    </select>
-                    <p>Item Type</p>
-                    <select
-                      id="itemType"
-                      {...register(`${fieldName}.itemType`, {
-                        valueAsNumber: true,
-                      })}
-                      onChange={handleChange}
-                    >
-                      <option value="0">AVATAR</option>
-                      <option value="1">INTERACTION</option>
-                      <option value="2">EMOTICON</option>
-                    </select>
-                    <p>Item Sub Type</p>
-                    {
-                      <select
-                        id="itemSubType"
-                        defaultValue={0}
-                        {...register(`${fieldName}.itemSubType`, {
-                          valueAsNumber: true,
-                        })}
+    <div>
+      <Container maxWidth="sm">
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="15vh"
+        >
+          <MailIcon
+            sx={{ fontSize: 80, align: "center" }}
+            align="right"
+            color="primary"
+          />
+        </Box>
+        <Container>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "100%" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="gold_input"
+              label="Gold"
+              variant="outlined"
+              value={data.gift.gold}
+              onChange={onChangeGold}
+              type="number"
+              InputProps={{ inputProps: { min: 0, max: 10000000 } }}
+            />
+            <TextField
+              id="diamond_input"
+              label="Diamond"
+              variant="outlined"
+              value={data.gift.diamond}
+              onChange={onChangeDiamond}
+              type="number"
+              InputProps={{ inputProps: { min: 0, max: 100000 } }}
+            />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Action</InputLabel>
+              <Select
+                labelId="action"
+                id="action"
+                value={data.gift.action}
+                label="Action"
+                onChange={onChangeAction}
+              >
+                <MenuItem value={"none"}>NONE</MenuItem>
+                <MenuItem value={"open_my_home"}>Open My Home</MenuItem>
+                <MenuItem value={"open_shop"}>Open Shop</MenuItem>
+                <MenuItem value={"join_table"}>Join Table</MenuItem>
+              </Select>
+            </FormControl>
+            {data.gift.tickets.length > 0 ? (
+              data.gift.tickets.map((ticket) => {
+                return (
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <FormControl style={{ width: "50%" }}>
+                      <InputLabel id="demo-simple-select-label">
+                        Ticket
+                      </InputLabel>
+                      <Select
+                        labelId="ticket"
+                        id="ticket"
+                        value={data.gift.tickets[0].ticketType}
+                        label="Ticket"
+                        onChange={onChangeTicketType}
                       >
-                        <option value="0">1 Day</option>
-                        <option value="1">7 Days</option>
-                        <option value="2">30 Days</option>
-                      </select>
-                    }
-                    <p>Item Number</p>
-                    <input
-                      type="number"
-                      defaultValue={1}
-                      id="itemNum"
-                      {...register(`${fieldName}.itemNum`, {
-                        valueAsNumber: true,
-                      })}
-                    ></input>
-                    <button type="button" onClick={removeItem(index)}>
-                      Remove
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </p>
-      <div>
-        <input type="button" value="+" onClick={clickAddItem} />
-      </div>
-      <p>
-        TICKETS
-        <div className="tickets">
-          <ul>
-            {tickets.map((index) => {
-              const fieldName = `tickets[${index}]`;
-              return (
-                <li>
-                  <div>
-                    <input
-                      type="number"
-                      defaultValue={0}
-                      id="ticketNum"
-                      {...register(`${fieldName}.ticketNum`, {
-                        valueAsNumber: true,
-                      })}
-                    ></input>
-                    <select
-                      id="ticketType"
-                      {...register(`${fieldName}.ticketType`, {
-                        valueAsNumber: true,
-                      })}
-                    >
-                      <option value="1">Event Egg Breaker</option>
-                      <option value="2">Event Lucky Card</option>
-                      <option value="3">Event Tacos</option>
-                    </select>
-                    <button type="button" onClick={removeTicket(index)}>
-                      Remove
-                    </button>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </p>
-      <div>
-        <input type="button" value="+" onClick={clickAddTicket} />
-      </div>
+                        <MenuItem value={1}>EggBreak</MenuItem>
+                        <MenuItem value={2}>LuckyCard</MenuItem>
+                        <MenuItem value={3}>Tacos</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <div style={{ width: "10px" }}></div>
+                    <FormControl style={{ width: "50%" }}>
+                      <TextField
+                        id="ticket_number_input"
+                        label="Number"
+                        variant="outlined"
+                        value={data.gift.tickets[0].ticketNum}
+                        onChange={onChangeTicketNum}
+                        type="number"
+                        InputProps={{ inputProps: { min: 0, max: 100000 } }}
+                      />
+                    </FormControl>
+                    <IconButton color="primary" component="label">
+                      <input hidden onClick={onRemoveTicket} />
+                      <DeleteIcon color="error" />
+                    </IconButton>
+                  </Box>
+                );
+              })
+            ) : (
+              <Button
+                variant="outlined"
+                onClick={onAddTicket}
+                color="primary"
+                startIcon={<AddIcon />}
+              >
+                ADD TICKET
+              </Button>
+            )}
+            <Button
+              onClick={onAddItem}
+              variant="outlined"
+              color="primary"
+              startIcon={<AddIcon />}
+            >
+              ADD ITEM
+            </Button>
+          </Box>
+        </Container>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="10vh"
+        >
+          <Button variant="contained" onClick={onSubmit}>
+            Generate
+          </Button>
+        </Box>
 
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "100%" },
+          }}
+        >
+          <ReactJson src={data} theme="monokai" />
+        </Box>
+      </Container>
       <NotificationContainer />
-      <input type="submit" />
-      <div className="App">
-        <ReactJson src={JsonConfig} theme="monokai" />
-      </div>
-    </form>
+    </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 
- 
-class Example extends React.Component {
-  createNotification = (type) => {
-    return () => {
-     
-    };
-  };
- 
-  render() {
-    return (
-      <div>
-        <button className='btn btn-info'
-          onClick={this.createNotification('info')}>Info
-        </button>
-        <hr/>
-        <button className='btn btn-success'
-          onClick={this.createNotification('success')}>Success
-        </button>
-        <hr/>
-        <button className='btn btn-warning'
-          onClick={this.createNotification('warning')}>Warning
-        </button>
-        <hr/>
-        <button className='btn btn-danger'
-          onClick={this.createNotification('error')}>Error
-        </button>
- 
-        <NotificationContainer/>
-      </div>
-    );
-  }
-}
- 
-ReactDOM.render(<><App />
-<Example/></>, rootElement);
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  rootElement
+);
